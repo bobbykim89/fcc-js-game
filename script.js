ed9e2bf508786c92eb32e4d728845d49ed66db62
Sihun Kim <skim585@asu.edu>
2024-04-29 16:44:15 -0700

    worked until end of lesson 16

--- script.js
+++ script.js
@@ -18,24 +18,47 @@ window.addEventListener('load', () => {
             this.speedY = 0
             this.dx = 0
             this.dy = 0
-            this.speedModifier = 20
+            this.speedModifier = 3
+            this.spriteWidth = 255
+            this.spriteHeight = 255
+            this.width = this.spriteWidth
+            this.height = this.spriteHeight
+            this.spriteX
+            this.spriteY
+            this.frameX = 0
+            this.frameY = 0
+            this.image = document.getElementById('bull')
         }
         draw(context) {
-            context.beginPath()
-            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2)
-            context.save()
-            context.globalAlpha = 0.5
-            context.fill()
-            context.restore()
-            context.stroke()
-            context.beginPath()
-            context.moveTo(this.collisionX, this.collisionY)
-            context.lineTo(this.game.mouse.x, this.game.mouse.y)
-            context.stroke()
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height)
+            if (this.game.debug) {
+                context.beginPath()
+                context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2)
+                context.save()
+                context.globalAlpha = 0.5
+                context.fill()
+                context.restore()
+                context.stroke()
+                context.beginPath()
+                context.moveTo(this.collisionX, this.collisionY)
+                context.lineTo(this.game.mouse.x, this.game.mouse.y)
+                context.stroke()
+            }
         }
         update() {
             this.dx = this.game.mouse.x - this.collisionX
             this.dy = this.game.mouse.y - this.collisionY
+            // sprite animation
+            const angle = Math.atan2(this.dy, this.dx)
+            if (angle < -2.74 || angle > 2.74)  this.frameY = 6
+            else if (angle < -1.96)  this.frameY = 7
+            else if (angle < -1.17)  this.frameY = 0
+            else if (angle < -0.39)  this.frameY = 1
+            else if (angle < 0.39)  this.frameY = 2
+            else if (angle < 1.17)  this.frameY = 3
+            else if (angle < 1.96)  this.frameY = 4
+            else if (angle < 2.74)  this.frameY = 5
+            
             const distance = Math.hypot(this.dy, this.dx)
             if (distance > this.speedModifier) {
                 this.speedX = this.dx / distance || 0
@@ -46,10 +69,23 @@ window.addEventListener('load', () => {
             }
             this.collisionX += this.speedX * this.speedModifier
             this.collisionY += this.speedY * this.speedModifier
+            this.spriteX = this.collisionX - this.width * 0.5
+            this.spriteY = this.collisionY - this.height * 0.5 - 100
+            // horizontal boundaries
+            if (this.collisionX < 0 + this.collisionRadius) this.collisionX =  this.collisionRadius
+            else if (this.collisionX > this.game.width - this.collisionRadius) this.collisionX = this.game.width - this.collisionRadius
+            // vertical boundaries
+            if (this.collisionY < this.game.topMargin + this.collisionRadius) this.collisionY = this.game.topMargin +this.collisionRadius
+            else if (this.collisionY > this.game.height - this.collisionRadius) this.collisionY = this.game.height - this.collisionRadius
             // collision with obstacles
             this.game.obstacles.forEach(obstacle => {
-                if (this.game.checkCollision(this, obstacle)) {
-                    console.log('collision')
+                // [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]
+                let [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, obstacle)
+                if (collision) {
+                    const unit_x = dx / distance
+                    const unit_y = dy / distance
+                    this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x
+                    this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y
                 }
             })
         }
@@ -60,7 +96,7 @@ window.addEventListener('load', () => {
             this.game = game
             this.collisionX = Math.random() * this.game.width
             this.collisionY = Math.random() * this.game.height
-            this.collisionRadius = 60
+            this.collisionRadius = 40
             this.image = document.getElementById('obstacles')
             this.spriteWidth = 250
             this.spriteHeight = 250
@@ -73,13 +109,15 @@ window.addEventListener('load', () => {
         }
         draw(context) {
             context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteWidth, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height)
-            context.beginPath()
-            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2)
-            context.save()
-            context.globalAlpha = 0.5
-            context.fill()
-            context.restore()
-            context.stroke()
+            if (this.game.debug) {
+                context.beginPath()
+                context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2)
+                context.save()
+                context.globalAlpha = 0.5
+                context.fill()
+                context.restore()
+                context.stroke()
+            }
         }
     }
 
@@ -89,6 +127,7 @@ window.addEventListener('load', () => {
             this.width = this.canvas.width
             this.height = this.canvas.height
             this.topMargin = 260
+            this.debug = false
             this.player = new Player(this)
             this.numberOfObstacles = 10
             this.obstacles = []
@@ -115,18 +154,21 @@ window.addEventListener('load', () => {
                     this.mouse.y = e.offsetY
                 }
             })
+            window.addEventListener('keydown', e => {
+                if (e.key === 'd') this.debug = !this.debug
+            })
         }
         render(context) {
+            this.obstacles.forEach(obstacle => obstacle.draw(context))
             this.player.draw(context)
             this.player.update()
-            this.obstacles.forEach(obstacle => obstacle.draw(context))
         }
         checkCollision(a, b) {
             const dx = a.collisionX - b.collisionX
             const dy = a.collisionY - b.collisionY
             const distance = Math.hypot(dy, dx)
             const sumOfRadii = a.collisionRadius + b.collisionRadius
-            return (distance < sumOfRadii)
+            return [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]
         }
         init() {
             let attempts = 0
@@ -143,7 +185,7 @@ window.addEventListener('load', () => {
                         overlap = true
                     }
                 })
-                const margin = testObstacle.collisionRadius * 2
+                const margin = testObstacle.collisionRadius * 3
                 if (!overlap && testObstacle.spriteX > 0 && testObstacle.spriteX < this.width - testObstacle.width && testObstacle.collisionY > this.topMargin + margin && testObstacle.collisionY < this.height - margin) {
                     this.obstacles.push(testObstacle)
                 }
